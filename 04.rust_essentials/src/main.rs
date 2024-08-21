e9e695038e7b2baabb1bbb444286605dc7551a77
Ahindra D <official.ahindra@gmail.com>
2024-08-22 02:05:40 +0530

    iterateSent_fn

--- 04.rust_essentials/src/main.rs
+++ 04.rust_essentials/src/main.rs
@@ -1,15 +1,19 @@
+
+
 fn main() {
-    let _signed_int: i32 = -555;
-    let _unsigned_int: u32 = 999;
+    // Variables - Strings, Integers, Floats, Boolean
+    let _if_valid: bool = true;
+    let _signed_int: i32 = -555; // can be both positive and negative - SIGNed
+    let mut _unsigned_int: u32 = 999; // to make it mutable  -  Only POSITIVE
     let _float_one: f32 = 3.1415;
     let sent_1 = String::from("hello world to the world of rust");
+
+    //Loops
     for i in 0..10 {
         if i % 2 == 1 {
             println!("{} - {}", sent_1, i);
         }
     }
-
-
 
     //GET Char At Index
     let nth_char = sent_1.chars().nth(300);
@@ -21,13 +25,19 @@ fn main() {
         None => println!("No char found at that index"),
     }
 
-
-
-
-    // let first_word = get_first_word(sentence);
-    // println!("{}", first_word)
+    //Function Call
+    let first_word = get_first_word(sent_1);
+    println!("first word: {}", first_word)
 }
 
-// fn get_first_word(sentence: String) -> String {
-
-// }
+fn get_first_word(sentence: String) -> String {
+    let mut res = String::new();
+    for char in sentence.chars() {
+        if char == ' ' {
+            break;
+        } else {
+            res.push_str(char.to_string().as_str());
+        }
+    }
+    return res;
+}
