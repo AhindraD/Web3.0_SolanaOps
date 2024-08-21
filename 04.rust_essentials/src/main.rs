afdb1963a3dc79ec660d0d14cbd37c72eeec8fa6
Ahindra D <official.ahindra@gmail.com>
2024-08-21 23:33:05 +0530

    loops_conditionals

--- 04.rust_essentials/src/main.rs
+++ 04.rust_essentials/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let _signed_int: i32 = -555;
+    let _unsigned_int: u32 = 999;
+    let _float_one: f32 = 3.1415;
+    let sent_1 = String::from("hello world");
+    for i in 0..100 {
+        if i % 2 == 1 {
+            println!("{} - {}", sent_1, i);
+        }
+    }
 }
