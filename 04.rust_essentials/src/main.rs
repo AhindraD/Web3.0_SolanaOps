374a4139adea28541c6ed52875aed32aae05659e
Ahindra D. <official.ahindra@gmail.com>
2024-12-19 22:56:06 +0530

    rust_bootcamp2

--- 04.rust_essentials/src/main.rs
+++ 04.rust_essentials/src/main.rs
@@ -1,43 +1,27 @@
-
-
 fn main() {
-    // Variables - Strings, Integers, Floats, Boolean
-    let _if_valid: bool = true;
-    let _signed_int: i32 = -555; // can be both positive and negative - SIGNed
-    let mut _unsigned_int: u32 = 999; // to make it mutable  -  Only POSITIVE
-    let _float_one: f32 = 3.1415;
-    let sent_1 = String::from("hello world to the world of rust");
-
-    //Loops
-    for i in 0..10 {
-        if i % 2 == 1 {
-            println!("{} - {}", sent_1, i);
-        }
-    }
-
-    //GET Char At Index
-    let nth_char = sent_1.chars().nth(300);
-    // println!("{}", nth_char.unwrap());  //FORECED - ciuld lear to runtime error
-
-    //IDEAL - pattern matching
-    match nth_char {
-        Some(x) => println!("{}", x),
-        None => println!("No char found at that index"),
-    }
-
-    //Function Call
-    let first_word = get_first_word(sent_1);
-    println!("first word: {}", first_word)
+    let nth: u32 = 9;
+    println!("n-th fib: {}", fib_nth(nth))
 }
 
-fn get_first_word(sentence: String) -> String {
-    let mut res = String::new();
-    for char in sentence.chars() {
-        if char == ' ' {
-            break;
-        } else {
-            res.push_str(char.to_string().as_str());
-        }
+fn fib_nth(n: u32) -> u32 {
+    let mut first: u32 = 0;
+    let mut second: u32 = 1;
+    let mut vec: Vec<u32> = Vec::new();
+
+    if n == 0 {
+        return first;
+    } else if n == 1 {
+        return second;
     }
-    return res;
+    vec.push(first);
+    vec.push(second);
+
+    for _i in 1..n {
+        let temp = first;
+        first = second;
+        second += temp;
+        vec.push(second);
+    }
+    println!("{:?}", vec);
+    return second;
 }
---
+++ 04.rust_essentials/src/var_loops.rs
@@ -0,0 +1,41 @@
+fn main() {
+    // Variables - Strings, Integers, Floats, Boolean
+    let _if_valid: bool = true;
+    let _signed_int: i32 = -555; // can be both positive and negative - SIGNed
+    let mut _unsigned_int: u32 = 999; // to make it mutable  -  Only POSITIVE
+    let _float_one: f32 = 3.1415;
+    let sent_1 = String::from("hello world to the world of rust");
+
+    //Loops
+    for i in 0..10 {
+        if i % 2 == 1 {
+            println!("{} - {}", sent_1, i);
+        }
+    }
+
+    //GET Char At Index
+    let nth_char = sent_1.chars().nth(300);
+    // println!("{}", nth_char.unwrap());  //FORECED - ciuld lear to runtime error
+
+    //IDEAL - pattern matching
+    match nth_char {
+        Some(x) => println!("{}", x),
+        None => println!("No char found at that index"),
+    }
+
+    //Function Call
+    let first_word = get_first_word(sent_1);
+    println!("first word: {}", first_word)
+}
+
+fn get_first_word(sentence: String) -> String {
+    let mut res = String::new();
+    for char in sentence.chars() {
+        if char == ' ' {
+            break;
+        } else {
+            res.push_str(char.to_string().as_str());
+        }
+    }
+    return res;
+}
