1f3e1a9c2ff411f24fcb98a18a9e300d85cf24c2
Ahindra D. <official.ahindra@gmail.com>
2025-02-27 06:51:29 +0530

    result enum

--- 04.rust_essentials/src/enum_with_val.rs
+++ 04.rust_essentials/src/enum_with_val.rs
@@ -1,3 +1,4 @@
+
 use std::f32::consts::PI;
 
 enum Shape {
@@ -6,19 +7,31 @@ enum Shape {
     Rect(f32, f32),
 }
 
-pub fn main() {
+fn main() {
     let shape_cir = Shape::Circle(13.3);
     let shape_sq = Shape::Square(9.0);
     let shape_rect = Shape::Rect(3.0, 5.0);
 
-    println!("Circle area: {}", cal_area(shape_cir));
-    println!("Square area: {}", cal_area(shape_sq));
-    println!("Rect area: {}", cal_area(shape_rect));
+    println!("Circle area: {}", shape_cir.cal_area());
+    println!("Square area: {}", shape_sq.cal_area());
+    println!("Rect area: {}", shape_rect.cal_area());
 }
-fn cal_area(s: Shape) -> f32 {
-    match s {
-        Shape::Circle(r) => PI * r * 2.0,
-        Shape::Square(a) => a * a,
-        Shape::Rect(x, y) => x * y,
+impl Shape {
+    fn cal_area(&self) -> f32 {
+        match self {
+            Shape::Circle(r) => PI * r * 2.0,
+            Shape::Square(a) => a * a,
+            Shape::Rect(x, y) => x * y,
+        }
     }
 }
+// fn cal_area(s: Shape) -> f32 {
+//     match s {
+//         Shape::Circle(r) => PI * r * 2.0,
+//         Shape::Square(a) => a * a,
+//         Shape::Rect(x, y) => x * y,
+//     }
+// }
+// println!("Circle area: {}", cal_area(shape_cir));
+// println!("Square area: {}", cal_area(shape_sq));
+// println!("Rect area: {}", cal_area(shape_rect));
