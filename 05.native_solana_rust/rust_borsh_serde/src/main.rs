90b56409c504c4144956adb4cd98faf410678822
Ahindra D <official.ahindra@gmail.com>
2025-03-08 01:15:36 +0530

    serializing struct

--- 05.native_solana_rust/rust_borsh_serde/src/main.rs
+++ 05.native_solana_rust/rust_borsh_serde/src/main.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use serde_json::{from_str, to_string};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserAccount {
@@ -11,4 +12,12 @@ fn main() {
         username: String::from("knite"),
         profile: String::from("x.com/knite"),
     };
+
+    //Seraialize to JSON
+    let serialized_json_str = to_string(&user_acc1).unwrap();
+    println!("{:?}", serialized_json_str);
+
+    //DE-Serialize to Struct
+    let deser_user_struct: UserAccount = from_str(&serialized_json_str).unwrap();
+    println!("{:?}", deser_user_struct);
 }
