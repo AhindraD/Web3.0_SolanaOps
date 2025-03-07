58d88ea1bb2c12a36e88c249a7a0750c3febc0eb
Ahindra D <official.ahindra@gmail.com>
2025-03-08 00:17:36 +0530

    serde

--- 05.native_solana_rust/rust_borsh_serde/src/main.rs
+++ 05.native_solana_rust/rust_borsh_serde/src/main.rs
@@ -1,3 +1,14 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UserAccount {
+    username: String,
+    profile: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user_acc1 = UserAccount {
+        username: String::from("knite"),
+        profile: String::from("x.com/knite"),
+    };
 }
